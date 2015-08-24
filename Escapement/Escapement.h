32e105b38196e83aa8e630f8224ab59684dba24d
Caleb Davenport <caleb@calebd.me>
2015-08-24 11:35:27 -0700

    Initial commit.

---
+++ Escapement/Escapement.h
@@ -0,0 +1,19 @@
+//
+//  Escapement.h
+//  Escapement
+//
+//  Created by Caleb Davenport on 8/24/15.
+//  Copyright (c) 2015 Hodinkee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Escapement.
+FOUNDATION_EXPORT double EscapementVersionNumber;
+
+//! Project version string for Escapement.
+FOUNDATION_EXPORT const unsigned char EscapementVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Escapement/PublicHeader.h>
+
+
