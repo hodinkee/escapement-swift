c40f0040a57b273018e6d6f3bd4ded5d37b5525d
Caleb Davenport <caleb@calebd.me>
2017-01-10 20:25:20 -0500

    Clean up Document a bit.

--- Escapement/Document.swift
+++ Escapement/Document.swift
@@ -16,31 +16,25 @@ public struct Document {
 
 extension Document {
     public func attributedString(stylesheet: Stylesheet) -> NSAttributedString {
-        let mutableAttributedString = NSMutableAttributedString(string: "")
-
-        for (index, paragraph) in paragraphs.enumerated() {
-            if index != 0 {
-                mutableAttributedString.append(NSAttributedString(string: "\n"))
-            }
-            mutableAttributedString.append(paragraph.attributedString(stylesheet: stylesheet))
-        }
-
-        return NSAttributedString(attributedString: mutableAttributedString)
+        return paragraphs.lazy
+            .map({ $0.attributedString(stylesheet: stylesheet) })
+            .joined(separator: { NSAttributedString(string: "\n") })
+            .reduce(NSAttributedString(string: ""), +)
     }
 }
 
-extension Document: Equatable {}
-
-public func ==(lhs: Document, rhs: Document) -> Bool {
-    return lhs.paragraphs == rhs.paragraphs
+extension Document: Equatable {
+    public static func == (lhs: Document, rhs: Document) -> Bool {
+        return lhs.paragraphs == rhs.paragraphs
+    }
 }
 
 
 // MARK: - DocumentDecoder
 
 public struct DocumentDecoder: DecoderType {
-    public static func decode(_ JSON: Alexander.JSON) -> Document? {
-        guard let paragraphs = JSON.decodeArray(ParagraphDecoder.self) else {
+    public static func decode(_ json: Alexander.JSON) -> Document? {
+        guard let paragraphs = json.decodeArray(ParagraphDecoder.self) else {
             return nil
         }
         return Document(paragraphs: paragraphs)
@@ -51,7 +45,22 @@ public struct DocumentDecoder: DecoderType {
 // MARK: - DocumentEncoder
 
 public struct DocumentEncoder: EncoderType {
-    public static func encode(_ value: Document) -> Any {
-        return ParagraphEncoder.encodeSequence(value.paragraphs)
+    public static func encode(_ document: Document) -> Any {
+        return ParagraphEncoder.encodeSequence(document.paragraphs)
     }
+}
+
+extension Sequence {
+    fileprivate func joined(separator: @escaping () -> Iterator.Element) -> AnySequence<Iterator.Element> {
+        return AnySequence(sequence(state: (makeIterator(), false), next: { (state: inout (Iterator, Bool)) -> Iterator.Element? in
+            defer { state.1 = !state.1 }
+            return state.1 ? separator() : state.0.next()
+        }))
+    }
+}
+
+fileprivate func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+    let string = NSMutableAttributedString(attributedString: lhs)
+    string.append(rhs)
+    return string
 }
