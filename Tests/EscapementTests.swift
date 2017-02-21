e1fedc0aabd131f92a4ea406ef3092a5d96e008a
Caleb Davenport <caleb@calebd.me>
2017-02-21 12:15:13 -0500

    Add tests.

--- Tests/EscapementTests.swift
+++ Tests/EscapementTests.swift
@@ -368,6 +368,22 @@ final class EscapementTests: XCTestCase {
 
         XCTAssertEqual(expected, document.attributedString(with: stylesheet))
     }
+
+    func testJSONEncodeDecode() {
+        guard let one = makeDocument(name: "test_overlapping_strong_emphasis_tags") else {
+            XCTFail("Missing document.")
+            return
+        }
+
+        let json = one.makeJSON()
+
+        guard let two = Document.init(json: json) else {
+            XCTFail("Missing document.")
+            return
+        }
+
+        XCTAssertEqual(one, two)
+    }
 }
 
 func makeDocument(name: String) -> Document? {
